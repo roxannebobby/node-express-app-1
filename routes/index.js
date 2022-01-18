9f1782ef2ad419a298f1c52773818766ed9ef860
Roxanne Weber <marie.sankofa@gmail.com>
2022-01-17 18:45:37 -0600

    added middleware to app page to use throughout all routes

--- routes/index.js
+++ routes/index.js
@@ -3,41 +3,63 @@ const turbo = require('turbo360')({ site_id: process.env.TURBO_APP_ID });
 const vertex = require('vertex360')({ site_id: process.env.TURBO_APP_ID });
 const router = vertex.router();
 
-// POST, GET, PUT, DELETE
-
-profiles = {
-	dkwon: {
-		name: 'dan kwon',
-		company: 'self',
-		languages: ['javascript', 'swift', 'python'],
-	},
-
+const profiles = {
 	sjobs: {
+		username: 'sjobs',
+		image: '/images/sjobs.jpg',
 		name: 'steve jobs',
 		company: 'apple',
 		languages: ['objective-c', 'swift', 'c++'],
 	},
-
 	bgates: {
+		username: 'bgates',
+		image: '/images/bgates.jpg',
 		name: 'bill gates',
 		company: 'microsoft',
-		languages: ['C', 'C#', 'Java'],
+		languages: ['c', 'c#', 'java'],
 	},
 };
 
-router.post('/addprofile', (req, res) => {
-	const body = req.body;
-	body['languages'] = req.body.languages.split(',');
-
-	profiles[body.username] = body;
-
-	res.redirect('/profile/' + body.username);
-});
+// POST, GET, PUT, DELETE
 
 router.get('/', (req, res) => {
 	res.render('index', {
 		text: 'This is the dynamic data. Open index.js from the routes directory to see.',
 	});
+});
+
+router.get('/profiles', (req, res) => {
+	const keys = Object.keys(profiles);
+	const list = [];
+	keys.forEach((key) => {
+		list.push(profiles[key]);
+	});
+
+	const data = {
+		profiles: list,
+	};
+
+	res.render('profiles', data);
+});
+
+router.post('/createprofile', (req, res) => {
+	const body = req.body;
+	body['languages'] = req.body.languages.split(', ');
+
+	profiles[body.username] = body;
+	res.json({
+		confirmation: 'success',
+		data: profiles[body.username],
+	});
+	// res.redirect('/profile/'+body.username)
+});
+
+router.post('/addprofile', (req, res) => {
+	const body = req.body;
+	body['languages'] = req.body.languages.split(', ');
+
+	profiles[body.username] = body;
+	res.redirect('/profile/' + body.username);
 });
 
 router.get('/query', (req, res) => {
@@ -65,14 +87,15 @@ router.get('/:profile/:username', (req, res) => {
 	const username = req.params.username;
 	const currentProfile = profiles[username];
 
-	if (!currentProfile) {
+	if (currentProfile == null) {
 		res.json({
 			confirmation: 'fail',
-			message: 'Profile ' + username + ' is not found.',
+			message: 'Profile ' + username + ' not found',
 		});
 
 		return;
 	}
+
 	res.render('profile', currentProfile);
 });
 
